5fbdf6bd302056d8ce731322e8bacabe356d3af8
Ilene <ileneh889@yahoo.com>
2025-02-20 10:51:34 +0800

    chore: relax ESLint rules

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -12,12 +12,12 @@ const stats = [
     text: "year of commitment"
   },
   {
-    num: 8,
+    num: 9,
     text: "Running projects"
   },
 
   {
-    num: 120,
+    num: 141,
     text: "Code commits"
   }
 ];
@@ -26,7 +26,7 @@ export default function Stats() {
   return (
     <section className='pt-4 pb-12 xl:pt-0 xl:pb-0'>
       <div className="container mx-auto">
-        <div className='flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max--w-none'>
+        <div className='grid grid-cols-2  md:grid-cols-4 gap-6 max-w-[80vw] mx-auto xl:max-w-none' >
           {stats.map((item, index) => (
             <div className="flex-1 flex gap-4 items-center justify-center xl:justify-start" key={index}>
               <CountUp
@@ -36,7 +36,7 @@ export default function Stats() {
                 className="text-4xl xl:text-6xl font-extrabold"
               />
               <p
-                className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-accent/80`}
+                className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80`}
               >
                 {item.text}
               </p>
